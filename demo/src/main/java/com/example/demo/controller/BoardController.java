e90e8e89f6f9ab346c1dac34c95def59b8d203d5
leedonghyeon <0928dlehdgus@gmail.com>
2025-04-15 17:58:06 +0900

    서버(서버전송 방법 submit-post (@Attribute, DTO, rttr) )

--- demo/src/main/java/com/example/demo/controller/BoardController.java
+++ demo/src/main/java/com/example/demo/controller/BoardController.java
@@ -2,9 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Log4j2
 @RequestMapping("/board")
@@ -15,6 +20,25 @@ public class BoardController {
     @GetMapping("/create")
     public void getCreater() {
         // return "./board/create";
+    }
+
+    @PostMapping("/create")
+    // public String postCreate(@ModelAttribute("name") String name,
+    // RedirectAttributes rttr) {
+    public void postCreate(String name, HttpSession session) {
+        log.info("name 가져오기 {} ", name);
+        session.setAttribute("name1", name);
+
+        // 어느 페이지로 이동을 하던지 간에 name 유지시키고 싶다면
+        // 커맨드객체, ModelAttribute(or @ModelAttribute)
+        // return "board/list";
+
+        // redirect 값 유지하고 이동
+
+        // rttr.addAttribute("name", name);
+        // rttr.addFlashAttribute("name", name);
+        // return "redirect:/board/list";
+
     }
 
     @GetMapping("/list")
--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -5,7 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.dto.LoginDTO;
+import com.example.demo.dto.MemberDTO;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Log4j2
 @RequestMapping("/member")
@@ -24,9 +34,41 @@ public class MemberController {
         log.info("회원가입");
     }
 
+    @PostMapping("/register")
+    public String postMethodName(@ModelAttribute("mDTO") MemberDTO memberDTO, RedirectAttributes rttr) {
+        log.info("회원가입 요청 {}", memberDTO);
+        // String userid = memberDTO.getUserid();
+        // String password = memberDTO.getPassword();
+        // boolean check = memberDTO.get;
+
+        // 로그인 페이지로 이동
+        // redirect 방식으로 가면서 값을 보내고 싶다면?
+        // rttr.addAttribute("userid", memberDTO.getUserid());
+        // /member/login?userid=1
+        rttr.addAttribute("userid", memberDTO.getUserid());
+        rttr.addFlashAttribute("password", memberDTO.getPassword());
+        return "redirect:/member/login";
+    }
+
     @GetMapping("/login")
     public void getLogin() {
         log.info("로그인");
+    }
+
+    @PostMapping("/login")
+    // public void postLogin(String userid, String password) {
+    // public void postLogin(LoginDTO loginDTO) {
+    public void postLogin(HttpServletRequest request) {
+        // HttpServletRequest : 사용자의 정보 및 서버 쪽 정보 추출
+
+        String userid = request.getParameter("userid");
+        String password = request.getParameter("password");
+        String remote = request.getRemoteAddr();
+        String local = request.getLocalAddr();
+
+        log.info("로그인 요청 {} {}", userid, password);
+        log.info("클라이언트 정보 {} {}", remote, local);
+        // template 찾기
     }
 
     @GetMapping("/logout")
