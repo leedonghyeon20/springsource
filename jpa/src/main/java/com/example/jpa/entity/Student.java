d93a1b0cb0d4d9c5825144ce2583b0c67ad8993a
leedonghyeon <0928dlehdgus@gmail.com>
2025-04-17 10:01:43 +0900

    서버 (4.16 jpa)

---
+++ jpa/src/main/java/com/example/jpa/entity/Student.java
@@ -0,0 +1,25 @@
+package com.example.jpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Table(name = "studenttbl")
+@Entity // == db table
+public class Student {
+
+    @Id
+    @SequenceGenerator(name = "student_seq_gen", sequenceName = "student_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_seq_gen")
+    // @GeneratedValue // create sequence studenttbl_seq start with 1 increment by
+    // 50
+    // @GeneratedValue(strategy = GenerationType.Auto)
+    private Long id; // id number(19,0) not null primary key (id)
+
+    @Column(name = "sname")
+    private String name; // name varchar2(255 char)
+}
