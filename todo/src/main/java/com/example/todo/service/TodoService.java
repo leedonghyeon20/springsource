41b35ea6fba3a5440b08d6a311a0ddb399c2d1cb
leedonghyeon <0928dlehdgus@gmail.com>
2025-04-23 18:03:14 +0900

    java - html (프로젝트 설계, 작성, DB확인, html연동)

---
+++ todo/src/main/java/com/example/todo/service/TodoService.java
@@ -0,0 +1,43 @@
+package com.example.todo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import com.example.todo.dto.ToDoDTO;
+import com.example.todo.entity.ToDo;
+import com.example.todo.repository.TodoRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    private final ModelMapper modelMapper;
+
+    public List<ToDoDTO> list(boolean completed) {
+        List<ToDo> list = todoRepository.findByCompleted(completed);
+        // ToDo entity => ToDoDTO 변경 후 리턴
+
+        // List<ToDoDTO> todos = new ArrayList<>();
+        // list.forEach(todo -> {
+        // ToDoDTO dto = modelMapper.map(todo, ToDoDTO.class);
+        // todos.add(dto);
+        // });
+
+        List<ToDoDTO> todos = list.stream()
+                .map(todo -> modelMapper.map(todo, ToDoDTO.class))
+                .collect(Collectors.toList());
+        return todos;
+
+    }
+
+}
---
+++ todo/src/test/java/com/example/todo/repository/TodoRepositoryTest.java
@@ -0,0 +1,61 @@
+package com.example.todo.repository;
+
+import java.util.stream.IntStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.todo.entity.ToDo;
+
+@SpringBootTest
+
+public class TodoRepositoryTest {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Test
+    public void testInsert() {
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+            ToDo toDo = ToDo.builder()
+                    .content("강아지 산책 " + i)
+                    .build();
+
+            todoRepository.save(toDo);
+        });
+    }
+
+    @Test
+    public void testRead() {
+        todoRepository.findAll().forEach(todo -> System.out.println(todo));
+    }
+
+    // todo 삭제
+    @Test
+    public void testDelete() {
+        todoRepository.deleteById(10L);
+    }
+
+    // todo 수정-완료
+    @Test
+    public void testUpdate() {
+        ToDo todo = todoRepository.findById(1L).get();
+
+        todo.setCompleted(true);
+        todoRepository.save(todo);
+    }
+
+    @Test
+    public void testRead2() {
+        // 완료 목록
+        todoRepository.findByCompleted(true)
+                .forEach(todo -> System.out.println(todo));
+    }
+
+    @Test
+    public void testRead3() {
+        // 중요 목록
+        todoRepository.findByImportanted(false)
+                .forEach(todo -> System.out.println(todo));
+    }
+}
