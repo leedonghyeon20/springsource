d10b0d6475335c5e916f9c4c6e8efe51b7435b80
leedonghyeon <0928dlehdgus@gmail.com>
2025-05-13 18:09:03 +0900

    DB sql 쿼리 (템플릿 양식 가져오기, 화면(html)설정 )

---
+++ movie/src/main/java/com/example/movie/controller/MovieController.java
@@ -0,0 +1,20 @@
+package com.example.movie.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Log4j2
+@Controller
+@RequestMapping("/movie")
+public class MovieController {
+
+    @GetMapping("/list")
+    public void getHome() {
+        log.info("영화리스트 요청");
+    }
+
+}
